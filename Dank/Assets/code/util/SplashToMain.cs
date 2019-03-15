10a47ccaae649bac34356ab7b41d2a3c4e91da46
netgibbon <netgibbon@gmail.com>
2019-03-15 23:35:43 +0000

    first thing

---
+++ Dank/Assets/code/util/SplashToMain.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashToMain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        StartCoroutine(LoadMenu());
+    }
+
+    // Update is called once per frame
+    IEnumerator LoadMenu()
+    {
+        yield return new WaitForSeconds(12);
+        SceneManager.LoadScene("MainMenu");
+    }
+}
